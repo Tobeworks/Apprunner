a89dba6b49941c395c27446b214d50ff40478124
Tobeworks <hallo@tobeworks.de>
2025-01-22 10:44:07 +0100

    flipbook removed

--- public/3d-flip-book/common/classes/targets/Target.js
+++
@@ -1,8 +0,0 @@
-
-export default class Target {
-
-  static test(object1, object2) {
-    return object1.target===object2.target;
-  }
-
-}
--- public/3d-flip-book/common/index.js
+++
@@ -1,5 +0,0 @@
-import {$} from './libs';
-
-
-
-window.jQuery = window.$ = $;
